7d24aed8fa11b85190456c803f85a18ec1b62416
Eya Cherni <eya.cherni.1@ens.etsmtl.ca>
2025-09-10 20:15:36 -0400

    flux google analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -19,7 +19,25 @@ export default function RootLayout({ children }: RootLayoutProps) {
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link href="https://fonts.googleapis.com/css2?family=Open+Sans:ital,wght@0,300..800;1,300..800&display=swap" rel="stylesheet" />
         <link rel="icon" type="image/jpg" href="/imgs/portfolio.png" />
-      </head>
+
+
+
+ {/* Google Analytics */}
+        <script
+          async
+          src="https://www.googletagmanager.com/gtag/js?id=G-V0NC0DQB5S"
+        ></script>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'G-V0NC0DQB5S');
+            `,
+          }}
+        />
+</head>
       <body>{children}</body>
     </html>
   );
